916333372f64563bae036c85c9c7316bec9f7132
Vital Sistemas <vital@MacBook-Air-de-Vital.local>
2015-11-05 15:55:41 -0600

    Subir Tarea 2

---
+++ modulo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  modulo2
+//
+//  Created by Vital Sistemas on 04/11/15.
+//  Copyright © 2015 Vital Sistemas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
